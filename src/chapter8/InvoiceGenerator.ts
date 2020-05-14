502ccaa7cb844e6833d4e87c92ebb247da96e24a
Felipe Secato <felipe.secato@gmail.com>
2020-05-14 12:40:10 -0300

    Refactor to use dependency injection

--- src/chapter8/InvoiceGenerator.ts
+++ src/chapter8/InvoiceGenerator.ts
@@ -1,11 +1,21 @@
 import Order from './Order';
 import Invoice from './Invoice';
 import InvoiceDao from './InvoiceDao';
+import SAP from './SAP';
 
 export default class InvoiceGenerator {
+  private dao: InvoiceDao;
+  private sap: SAP;
+
+  constructor(invoiceDao: InvoiceDao, sap: SAP) {
+    this.dao = invoiceDao;
+    this.sap = sap;
+  }
+
   public generate(order: Order): Invoice {
     const invoice = new Invoice(order.client, order.total * 0.94, new Date());
-    new InvoiceDao().persist(invoice);
+    this.dao.persist(invoice);
+    this.sap.send(invoice);
     return invoice;
   }
 }
--- tests/chapter8/invoiceGenerator.test.ts
+++ tests/chapter8/invoiceGenerator.test.ts
@@ -1,23 +1,27 @@
 import Order from '../../src/chapter8/Order';
 import InvoiceGenerator from '../../src/chapter8/InvoiceGenerator';
 import InvoiceDao from '../../src/chapter8/InvoiceDao';
+import SAP from '../../src/chapter8/SAP';
 jest.mock('../../src/chapter8/InvoiceDao');
-
+jest.mock('../../src/chapter8/SAP');
 beforeEach(() => {
   (InvoiceDao as any).mockClear();
+  (SAP as any).mockClear();
 });
 
 describe('Invoice generator class', () => {
   test('Should generate invoice with taxes discounted', () => {
-    const invoiceGenerator = new InvoiceGenerator();
+    const invoiceGenerator = new InvoiceGenerator(new InvoiceDao(), new SAP());
     const order = new Order('Felipe', 100, 1);
     const invoice = invoiceGenerator.generate(order);
 
     expect(invoice.value).toBe(100 * 0.94);
     expect(InvoiceDao).toHaveBeenCalledTimes(1);
+    expect(SAP).toHaveBeenCalledTimes(1);
   });
 
   test('Clear mock is working', () => {
     expect(InvoiceDao).not.toHaveBeenCalled();
+    expect(SAP).not.toHaveBeenCalled();
   });
 });
